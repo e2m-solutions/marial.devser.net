d81908d86ef4f7cae432482bbb1d857e9b157e9f
Pankajbera17 <109953003+Pankajbera17@users.noreply.github.com>
2022-08-04 14:20:03 +0530

    css

--- wp-content/themes/legendary-toolkit-child/page-templates/home-page-template.php
+++ wp-content/themes/legendary-toolkit-child/page-templates/home-page-template.php
@@ -26,6 +26,27 @@
                 <?php  }
                 ?>
             </div>
+            <div class="hero-btn d-flex">
+                <?php 
+                    $link = get_field('hero_button_1');
+                    if( $link ): 
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                        ?>
+                        <a class="btn-white" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <?php endif; ?>
+
+                <?php 
+                    $link = get_field('hero_button_2');
+                    if( $link ): 
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                        ?>
+                        <a class="btn-transparent" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </section>
