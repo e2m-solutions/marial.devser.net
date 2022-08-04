31e41129aad00007251f7512ef454760bb9a009f
Pankajbera17 <109953003+Pankajbera17@users.noreply.github.com>
2022-08-04 16:08:25 +0530

    code

--- wp-content/themes/legendary-toolkit-child/footer.php
+++ wp-content/themes/legendary-toolkit-child/footer.php
@@ -9,44 +9,46 @@
  * @package WP_Bootstrap_Starter
  */
 ?>
-<?php $image = get_field('footer_background_image', 'option');
-      $cta_background = get_field('cta_background', 'option');
-      $cta_image = get_field('cta_image', 'option');
-      $cta_content = get_field('cta_content', 'option');
+<?php $title = get_field('cta_content', 'option');
+      $content = get_field('cta_image', 'option');
+      $button = get_field('cta_button', 'option');
 ?>
 <?php if (!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )) : ?>
 			</div><!-- .row -->
 		</div><!-- .container -->
 
-	<?php if (!is_page('258')) {?>
-		<section class="help-section-cover" style="background-image: url('<?php echo $cta_background['url'] ?>');">
+	<?php if (!is_page('')) {?>
+		<section class="cta-section">
 			<div class="container">
-				<div class="help-content-cover d-flex">
-				<?php if($cta_image) {?>
-
-						<div class="left-side-part">
-							<img src="<?php echo $cta_image['url'] ?>" alt="<?php echo $cta_image['alt'] ?>">
-						</div>
-						<div class="right-content">
-							<?php if($cta_content){ ?>
-							<?php  echo $cta_content ?>
-						<?php	} ?>
-						<div class="talk-btn">
-								<?php
-							$link = get_field('cta_button', 'option');
-								if( $link ): 
-									$link_url = $link['url'];
-									$link_title = $link['title'];
-									$link_target = $link['target'] ? $link['target'] : '_self';
-									?>
-									<a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-								<?php endif; ?>
-						</div>
-						</div>
-				<?php }  ?>
+				<div class="cta-content-wrap text-center">
+					<div class="cta-title">
+						<?php $title = get_field('title');
+							if($title){ ?>
+								<span><?php echo $title; ?></span>
+							<?php  }
+						?>
+					</div>
+					<div class="cta-content text-center">
+						<?php $content = get_field('content');
+							if($content){ ?>
+								<span><?php echo $content; ?></span>
+							<?php  }
+						?>
+					</div>
+					<div class="cta-button">
+						<?php 
+						$link = get_field('cta_button');
+							if( $link ): 
+								$link_url = $link['url'];
+								$link_title = $link['title'];
+								$link_target = $link['target'] ? $link['target'] : '_self';
+								?>
+								<a class="btn-blue" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+					<?php endif; ?>
+					</div>
 				</div>
 			</div>
-		</section>	
+		</section>
 	<?php	} ?>	
 		<div id="footer" style="background-image: url('<?php echo $image['url'] ?>');">
 			<?php get_template_part('template-parts/footer', 'widgets');?>
