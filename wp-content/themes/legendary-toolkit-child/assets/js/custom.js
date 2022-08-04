bbae7325179b96983b76ec6a0f39d6fb63e3aa47
Pankajbera17 <109953003+Pankajbera17@users.noreply.github.com>
2022-08-04 19:01:46 +0530

    ff

--- wp-content/themes/legendary-toolkit-child/assets/js/custom.js
+++ wp-content/themes/legendary-toolkit-child/assets/js/custom.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function () {
     jQuery('.testimonial-wrap').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
       });
 });
