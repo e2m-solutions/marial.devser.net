89f90b3b4e3078f997f6aee774b7f60aa4ea16c0
Pankajbera17 <109953003+Pankajbera17@users.noreply.github.com>
2022-08-04 18:58:52 +0530

    fff

--- wp-content/themes/legendary-toolkit-child/functions.php
+++ wp-content/themes/legendary-toolkit-child/functions.php
@@ -22,7 +22,7 @@ function legendary_toolkit_child_enqueue_styles() {
 add_action('wp_head', 'legendary_add_js_scripts');
 function legendary_add_js_scripts() {
     wp_enqueue_script( 'script-js', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array(), '1.0.0', true );
-    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/assets/js/custom.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/assets/js/custom.js', array('jquery'), true );
 }
 
 
